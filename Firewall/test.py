f95858b5cb746e4a62900756630087f6e92d5b67
ubuntu <ubuntu@ubuntu>
2012-02-15 12:50:00 +0100

    Selected timestamp from log entry

---
+++ Firewall/test.py
@@ -0,0 +1,30 @@
+'''
+Created on Feb 15, 2012
+
+@author: ubuntu
+'''
+filer="/home/ubuntu/access.log"
+f=open(filer,"r")
+filew="/home/ubuntu/access.log.bak"
+g=open(filew,"w")
+while 1:
+    line=f.readline()
+    if not line: break
+    i=0
+    c=0
+    for i in line:
+        c=c+1
+    i=0
+    while 1:
+        if(line[i]=='.'):
+            time=int(line[0:i])
+        if(line[i]==' '):
+            break
+        i=i+1
+    while line[i]==' ':
+        i=i+1
+
+    
+    
+        
+        
