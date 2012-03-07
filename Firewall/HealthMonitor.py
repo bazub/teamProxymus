8e222a98b5a654b2eb6b16689d95fb6aaea11d26
Bogdan Bazu <bazu_b@yahoo.com>
2012-03-07 13:39:37 +0200

    blablabla

--- Firewall/HealthMonitor.py
+++ Firewall/HealthMonitor.py
@@ -9,7 +9,7 @@ root.title("HM HealthMonitor©                                                  
 root.bind("<Escape>", lambda e: e.widget.quit())
 root.resizable(FALSE,FALSE)
 def PIcomm():
-    a=subprocess.call("PING 10.140.32.75")
+    a=subprocess.call("PING 10.140.16.47")
     if a:
         showwarning("ERROR!!","Your Apache server is down!")
     else:
@@ -17,6 +17,6 @@ def PIcomm():
 
 butPI=Button(root, text="Check Apache", command=PIcomm,width="20")
 butPI.grid(row=0,column=1)
-
+subprocess.call("scp root@10.140.16.47:/var/log/squid/access.log C:/access.log")
 
 root.mainloop()    
